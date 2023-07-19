1cf4f9c4a037dbfaabc1778b1aecf1934afefa9d
rodric74 <rodric.rambosson@gmail.com>
2023-07-19 14:30:35 +0200

    ajustement Home pr Header et Footer

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Header from '../../components/Header/header';
 import Banner from '../../components/Banner/banner';
-import Footer from '../../components/Footer/footer';
 import Card from '../../components/Card/card';
 import propertiesData from '../../datas/logements.json';
 import './Home.scss';
