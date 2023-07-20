4241b46c36b6b357b491ee5c0be6e9eb9392053e
rodric74 <rodric.rambosson@gmail.com>
2023-07-20 21:38:53 +0200

    slideshow brut ok

--- src/components/Slideshow/slideshow.jsx
+++ src/components/Slideshow/slideshow.jsx
@@ -1,7 +1,38 @@
+import React, { useState } from "react";
+import './Slideshow.scss'
+import slideShowArrow from '../../assets/Vector.svg'
 
+const Slideshow = ({ images }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-function Slideshow() {
-  return <div>Slideshow Component</div>;
-}
+  const nextSlide = () => {
+    setCurrentSlide((currentSlide + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((currentSlide - 1 + images.length) % images.length);
+  };
+
+  if (images.length === 0) return null;
+
+  return (
+    <div className="slideshow">
+      {images.length > 1 && (
+        <>
+          <button onClick={prevSlide} className="slideshow__button slideshow__button--prev">
+            <img src={slideShowArrow} alt="Précédent" />
+          </button>
+          <button onClick={nextSlide} className="slideshow__button slideshow__button--next">
+            <img src={slideShowArrow} alt="Suivant" />
+          </button>
+        </>
+      )}
+      <img src={images[currentSlide]} alt="" className="slideshow__image" />
+      {images.length > 1 && (
+        <div className="slideshow__counter">{currentSlide + 1} / {images.length}</div>
+      )}
+    </div>
+  );
+};
 
 export default Slideshow;
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,12 +3,22 @@ import Banner from '../../components/Banner/banner';
 import homeImage from '../../assets/kasa-banner-ocean.jpg'
 import Card from '../../components/Card/card';
 import propertiesData from '../../datas/logements.json';
+import Slideshow from '../../components/Slideshow/slideshow';
 import './Home.scss';
 
 function Home() {
+  const images = [
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-1.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-2.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-3.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-4.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-5.jpg",
+			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-6.jpg"
+  ];
   return (
     <div>
       <Banner backgroundImage={homeImage} />
+      <Slideshow images={images} />
       <div className="card-container">
         {propertiesData.map((property) => (
           <Card key={property.id} property={property} />
