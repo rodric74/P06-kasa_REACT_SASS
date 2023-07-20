b323a9295224a0fe7db23e454f0679d3bf279fd4
rodric74 <rodric.rambosson@gmail.com>
2023-07-20 14:32:53 +0200

    Component Collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import arrowIcon from '../../assets/arrow_back_ios-24px 2.svg'
 
+function Collapse({ title, content}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
 
-function Collapse() {
-  return <div>Collapse Component</div>;
+  };
+
+  return (
+    <div>
+      <div>
+        {title}
+        <img src={arrowIcon} 
+              alt="flèche pour ouverture Collapse" 
+              onClick={handleToggle} 
+              style={{transform: isOpen ? 'rotate(90deg)' : 'none'}} 
+        />
+      </div>
+      {isOpen && <div>{content}</div> }
+    </div>
+  );
 }
 
 export default Collapse;
