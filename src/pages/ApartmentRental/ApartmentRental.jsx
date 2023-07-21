2fb290cfceda69554b755c6a845920a3fbc06d3f
rodric74 <rodric.rambosson@gmail.com>
2023-07-21 13:14:36 +0200

    page ApartementRental workin progress

--- src/pages/ApartmentRental/ApartmentRental.jsx
+++ src/pages/ApartmentRental/ApartmentRental.jsx
@@ -1,9 +1,35 @@
+import { useParams } from 'react-router-dom';
+import propertiesData from '../../datas/logements.json';
+import Slideshow from '../../components/Slideshow/slideshow';
+import Rating from '../../components/Rating/rating';
+import Owner from '../../components/Owner/owner';
+import Property from '../../components/Property/property';
+import Collapse from '../../components/Collapse/collapse';
 import './ApartmentRental.scss'
 
-const ApartmentRental = () => {
-    return(
-        <div>ApartmentRental</div>
-    );
+function ApartmentRental() {
+  const { id } = useParams(); 
+  const property = propertiesData.find(p => p.id === id);
+  if (!property) {
+    return <div>Property not found</div>;
+  }
+
+  return (
+    <div className='apartement-rental'>
+      <Slideshow images={property.pictures} />
+      <div className='apartement-rental__property'>
+      <Property property={property} />
+      <div className='appartement-rental__owner'>
+      <Owner host={property.host} />
+      <Rating rating={property.rating} />
+      </div>
+      </div>
+      <div className="apartement-rental__collapse">
+        <Collapse title="Description" content={property.description} className="collapse collapse--apartement" />
+        <Collapse title="Equipments" content={property.equipments.join(', ')} className="collapse collapse--apartement" />
+      </div>
+    </div>
+  );
 }
 
 export default ApartmentRental;
--- src/pages/Property/property.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Property() {
-  return <div>Property Component</div>;
-}
-
-export default Property;
