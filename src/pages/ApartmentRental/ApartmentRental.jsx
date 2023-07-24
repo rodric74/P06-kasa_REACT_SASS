1d119af2047a71093f8d67abea0417c8332115b7
rodric74 <rodric.rambosson@gmail.com>
2023-07-24 22:59:15 +0200

    utiliser Navigate dans ApartementRental.jsx

--- src/pages/ApartmentRental/ApartmentRental.jsx
+++ src/pages/ApartmentRental/ApartmentRental.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import propertiesData from '../../datas/logements.json';
 import Slideshow from '../../components/Slideshow/slideshow';
 import Rating from '../../components/Rating/rating';
@@ -10,28 +10,28 @@ import './ApartmentRental.scss'
 function ApartmentRental() {
   const { id } = useParams(); 
   const property = propertiesData.find(p => p.id === id);
+  
   if (!property) {
-    return <div>Property not found</div>;
+    return <Navigate to="/error" />;
   }
 
   return (
     <div className='apartement-rental'>
       <Slideshow images={property.pictures} />
       <div className='apartement-rental__property'>
-      <Property property={property} />
-      <div className='appartement-rental__owner'>
-      <Owner host={property.host} />
-      <Rating rating={property.rating} />
-      </div>
+        <Property property={property} />
+        <div className='appartement-rental__owner'>
+          <Owner host={property.host} />
+          <Rating rating={property.rating} />
+        </div>
       </div>
       <div className="apartement-rental__collapse">
         <Collapse title="Description" content={property.description} className="collapse collapse--apartement" />
         <Collapse 
-  title="Equipments" 
-  content={property.equipments.map((equipment, index) => <span key={index} className="equipment-item">{equipment}</span>)} 
-  className="collapse collapse--apartement" 
-/>
-
+          title="Equipments" 
+          content={property.equipments.map((equipment, index) => <span key={index} className="equipment-item">{equipment}</span>)} 
+          className="collapse collapse--apartement" 
+        />
       </div>
     </div>
   );
