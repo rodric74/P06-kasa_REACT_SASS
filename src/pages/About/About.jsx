6f6a4d3d0f8f80641ccb0468f5320eb61d359ae5
rodric74 <rodric.rambosson@gmail.com>
2023-07-24 17:38:03 +0200

    refonte index.jsx, card-cointainer en grid, + cosmetique

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,17 +6,17 @@ function About() {
   return (
   <div>
     <Banner backgroundImage={aboutImage} />
-    <div className='about'>
+    <div className='about--test'>
     <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
- régulièrement vérifiées par nos équipes." className="collapse-about" />
+ régulièrement vérifiées par nos équipes." className="collapse--about" />
     <Collapse title='Respect' content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-perturbation du voisinage entraînera une exclusion de notre plateforme. " className="collapse-about" />
+perturbation du voisinage entraînera une exclusion de notre plateforme. " className="collapse--about" />
     <Collapse title='Service' content="lLa bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-perturbation du voisinage entraînera une exclusion de notre plateforme." className="collapse-about" />
+perturbation du voisinage entraînera une exclusion de notre plateforme." className="collapse--about" />
     <Collapse title='Sécurité' content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
 correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
 locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
-également des ateliers sur la sécurité domestique pour nos hôtes." className="collapse-about" />
+également des ateliers sur la sécurité domestique pour nos hôtes." className="collapse--about" />
     </div>
   </div>);
 }
