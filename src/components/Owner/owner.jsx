e50b3fa48cf577c1e5a6e019da80d76e38afc7da
rodric74 <rodric.rambosson@gmail.com>
2023-07-24 22:27:25 +0200

    changer nom App.scss pr bug Github

--- src/components/Owner/owner.jsx
+++ src/components/Owner/owner.jsx
@@ -1,9 +1,15 @@
 import './Owner.scss';
 
 const Owner = ({ host }) => {
+  // Split the name into first name and last name
+  const [firstName, lastName] = host.name.split(' ');
+
   return (
     <div className="owner">
-      <div className="owner__name">{host.name}</div>
+      <div className="owner__name">
+        <div>{firstName}</div>
+        <div>{lastName}</div>
+      </div>
       <img className="owner__picture" src={host.picture} alt={host.name} />
     </div>
   );
