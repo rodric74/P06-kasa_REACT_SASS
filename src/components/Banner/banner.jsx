8665b2ff75daff53c4c957922022d44f750224ff
rodric74 <rodric.rambosson@gmail.com>
2023-07-25 16:39:14 +0200

    texte bannière responsive Figma

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -3,7 +3,7 @@ import './Banner.scss'
 const Banner = ({ backgroundImage }) => {
     return (
         <div className='banner' style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <h1 className='banner__text'>Chez vous, partout et ailleurs</h1>
+            <h1 className='banner__text'><span>Chez vous,</span> <span>partout et ailleurs</span></h1>
         </div>
     )
 }
